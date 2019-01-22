f8d3c6fd5a4bf3791fa56691c20d61eb20288b07
Paul <bielcu@gmail.com>
2019-01-22 18:17:44 +0100

    implement fields, addChild, getChild and removeChild methods to TrieDataNode class

--- src/main/java/com/codecool/javatries/TrieDataNode.java
+++ src/main/java/com/codecool/javatries/TrieDataNode.java
@@ -1,15 +1,23 @@
 package com.codecool.javatries;
 
+import sun.text.normalizer.Trie;
+
+import javax.xml.soap.Node;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TrieDataNode {
 
     private char data;
-    // TODO add further members as needed
+    //private TrieDataNode parent;
+    private Set<TrieDataNode> children;
+    private String word;
 
-    /**
-     * Initializes a TrieDataNode with its data
-     * @param data The data in this node
-     */
+    private TrieDataNode() {
+    }
+
     public TrieDataNode(char data) {
+        children = new HashSet<>();
         this.data = data;
     }
 
@@ -22,4 +30,33 @@ public class TrieDataNode {
         return Character.toString(data);
     }
 
+    /*public TrieDataNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TrieDataNode parent) {
+        this.parent = parent;
+    }*/
+
+    public TrieDataNode getChild(char character) {
+        TrieDataNode childNode = null;
+        for (TrieDataNode n : children) {
+            if (n.getData() == character) {
+                childNode = n;
+            }
+        }
+        return childNode;
+    }
+
+    public void addChild(TrieDataNode newNode) {
+        children.add(newNode);
+    }
+
+    public void removeChild(char character) {
+        for (TrieDataNode n : children) {
+            if (n.getData() == character) {
+                children.remove(n);
+            }
+        }
+    }
 }
